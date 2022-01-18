2a1bd2098dd9a52694147a8ef9691ce2c1ae8997
Aliaksei Tkachou <you@example.com>
2022-01-18 21:52:20 +0300

    first step...

--- source/internet.h
+++ source/internet.h
@@ -1,2 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <bitset>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <array>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <ios>
+#include <typeinfo>
+#include <utility>
+#include <random>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "all_sorts.cpp"
-#include "some\ structs/Tqueue.h"
+#include "some_structs/Tqueue.h"
+#include "some_structs/Tstack.h"
