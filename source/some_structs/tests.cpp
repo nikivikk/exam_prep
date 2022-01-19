d4b7a8bdd39b73505e88560f03a51c74507fde2b
Aliaksei Tkachou <you@example.com>
2022-01-19 04:44:52 +0300

    idk trash, let it be

--- source/some_structs/tests.cpp
+++ source/some_structs/tests.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+#include "Tlist.h"
+#include "Tdeque.h"
 #include "Tqueue.h"
 #include "Tstack.h"
 
 using namespace std;
 
-int main()
+int mai2n()
 {
     Tstack<int> st;
     st.push(234);
@@ -18,5 +20,61 @@ int main()
     st.pop();
     st.pop();
     st.pop();
+    return 0;
+}
+
+int mainnn()
+{
+    Tqueue<int> q;
+    q.push(12);
+    q.push(23);
+    q.print();
+    q.pop_first();
+    q.print();
+    q.pop_first();
+    q.pop_first();
+    q.pop_first();
+//    cout << q;
+    return 0;
+}
+
+int maihn()
+{
+    Tdeque<int> d;
+    d.push_front(13);
+    d.print();
+    d.push_front(122);
+    d.push_back(12);
+    d.push_back(1);
+    d.print();
+    cout << '\n' << d.size() << '\n';
+    d.pop_last();
+    d.print();
+    d.pop_first();
+    d.pop_last();
+    d.pop_last();
+    d.pop_last();
+    d.pop_last();
+    d.pop_last();
+    d.pop_last();
+    d.pop_last();
+    return 0;
+}
+
+int main()
+{
+    Tlist<int> l;
+    l.push_back(12);
+    l.push_back(16);
+    l.push_back(15);
+    l.push_back(14);
+    l.push_back(13);
+    l.print();
+    cout << '\n' << l.size() << '\n';
+    l.resize(7);
+    cout << '\n' << l.size() << '\n';
+    l.print();
+    l.insert(l.size(), 3);
+    l.print();
     return 0;
 }
--- source/testsss.cpp
+++ source/testsss.cpp
@@ -1,5 +1,11 @@
 #include "internet.h"
+
+
+
 int main()
 {
+    string t = "asd, asd, asd, asd,,, m, asd,,, asd";
+    cout << SplitString(t, ", ");
+   
     return 0;
 }
